68b8270c86bc62721181cf725b9d929fbe6c36e4
Tocutoeltuco <tocutoeltuco@gmail.com>
2018-12-23 23:44:09 -0300

    uploaded the files

---
+++ deps/fromage/package.lua
@@ -0,0 +1,19 @@
+return {
+	name = "Lautenschlager-id/fromage",
+	version = "0.2.5",
+	homepage = "https://github.com/Lautenschlager-id/Fromage",
+	dependencies = {
+		"creationix/coro-http@3.0.0",
+		"creationix/coro-websocket@3.1.0",
+		"luvit/secure-socket@1.2.0"
+	},
+	tags = { "atelier801", "forum", "transformice", "api" },
+	license = "MIT",
+	author = "Lautenschlager.id",
+	files = {
+		"init.lua",
+		"package.lua",
+		"libs/enum.lua",
+		"libs/enumerations.lua"
+	}
+}
